4d6c24e79bf3324af03970318e74cb62fd4c4019
Ran Duan <rnduan@gmail.com>
2016-08-24 12:57:05 -0400

    Player Controller

--- browser/js/album/album.controllers.js
+++ browser/js/album/album.controllers.js
@@ -1,7 +1,7 @@
 /* global juke */
 'use strict';
 
-juke.controller('AlbumCtrl', function ($scope, $http, $rootScope, $log, StatsFactory, AlbumFactory) {
+juke.controller('AlbumCtrl', function ($scope, $http, $rootScope, $log, StatsFactory, AlbumFactory, PlayerFactory) {
 
   // load our initial data
   AlbumFactory.fetchAll()
@@ -24,40 +24,53 @@ juke.controller('AlbumCtrl', function ($scope, $http, $rootScope, $log, StatsFac
 
   // main toggle
   $scope.toggle = function (song) {
-    if ($scope.playing && song === $scope.currentSong) {
-      $rootScope.$broadcast('pause');
+    if (song === PlayerFactory.getCurrentSong() && PlayerFactory.isPlaying()) {
+      PlayerFactory.pause();
+    } else if (song === PlayerFactory.getCurrentSong()) {
+      PlayerFactory.resume();
     } else {
-      $rootScope.$broadcast('play', song);
+      PlayerFactory.start(song);
     }
   };
 
   // incoming events (from Player, toggle, or skip)
-  $scope.$on('pause', pause);
-  $scope.$on('play', play);
-  $scope.$on('next', next);
-  $scope.$on('prev', prev);
+  // $scope.$on('pause', pause);
+  // $scope.$on('play', play);
+  // $scope.$on('next', next);
+  // $scope.$on('prev', prev);
 
   // functionality
-  function pause () {
-    $scope.playing = false;
-  }
-  function play (event, song) {
-    $scope.playing = true;
-    $scope.currentSong = song;
-  }
+  // function pause () {
+  //   $scope.playing = false;
+  // }
+  // function play (event, song) {
+  //   $scope.playing = true;
+  //   $scope.currentSong = song;
+  // }
 
-  // a "true" modulo that wraps negative to the top of the range
-  function mod (num, m) { return ((num % m) + m) % m; }
+  // // a "true" modulo that wraps negative to the top of the range
+  // function mod (num, m) { return ((num % m) + m) % m; }
 
-  // jump `interval` spots in album (negative to go back, default +1)
-  function skip (interval) {
-    if (!$scope.currentSong) return;
-    var index = $scope.currentSong.albumIndex;
-    index = mod( (index + (interval || 1)), $scope.album.songs.length );
-    $scope.currentSong = $scope.album.songs[index];
-    if ($scope.playing) $rootScope.$broadcast('play', $scope.currentSong);
-  }
-  function next () { skip(1); }
-  function prev () { skip(-1); }
+  // // jump `interval` spots in album (negative to go back, default +1)
+  // function skip (interval) {
+  //   if (!$scope.currentSong) return;
+  //   var index = $scope.currentSong.albumIndex;
+  //   index = mod( (index + (interval || 1)), $scope.album.songs.length );
+  //   $scope.currentSong = $scope.album.songs[index];
+  //   if ($scope.playing) $rootScope.$broadcast('play', $scope.currentSong);
+  // }
+  // function next () { skip(1); }
+  // function prev () { skip(-1); }
 
+});
+
+
+juke.controller('AlbumsCtrl', function($scope, AlbumFactory) {
+  AlbumFactory.fetchAll()
+  .then(function(albums) {
+    $scope.albums = albums;
+    albums.forEach(function(album) {
+      album.imageUrl = '/api/albums/' + album.id + '/image';
+    });
+  });
 });
--- browser/js/player/player.factory.js
+++ browser/js/player/player.factory.js
@@ -1,5 +1,65 @@
 'use strict';
 
-juke.factory('PlayerFactory', function(){
-  // non-UI logic in here
+juke.factory('PlayerFactory', function($rootScope){
+  var playerObj = {};
+
+  // State
+  playerObj.currentSong;
+  playerObj.playing = false;
+  playerObj.songList;
+
+  playerObj.audio = document.createElement('audio');
+
+  playerObj.start = function(song, songList) {
+    this.songList = songList;
+
+    // stop existing audio (e.g. other song) in any case
+    this.pause();
+    this.playing = true;
+
+    // resume current song
+    if (song === this.currentSong) return this.audio.play();
+
+    // enable loading new song
+    this.currentSong = song;
+    this.audio.src = song.audioUrl;
+    this.audio.load();
+    this.audio.play();
+  };
+  playerObj.pause = function() {
+    if (!this.audio) return;
+    this.audio.pause();
+    this.playing = false;
+  };
+  playerObj.resume = function() {
+    if (!this.audio) return;
+    this.audio.play();
+    this.playing = true;
+  };
+  playerObj.isPlaying = function() {
+    return this.playing;
+  };
+  playerObj.getCurrentSong = function() {
+    if (!this.currentSong) return null;
+    return this.currentSong;
+  };
+  playerObj.next = function() {
+    this.pause();
+    if (!this.songList) return;
+    var i = this.songList.indexOf(this.currentSong) + 1;
+    if (i > this.songList.length - 1) i = 0;
+    this.start(this.songList[i], this.songList);
+  };
+  playerObj.previous = function() {
+    this.pause();
+    if (!this.songList) return;
+    var i = this.songList.indexOf(this.currentSong) - 1;
+    if (i < 0) i = this.songList.length - 1;
+    this.start(this.songList[i], this.songList);
+  };
+  playerObj.getProgress = function() {
+    if (!this.currentSong) return 0;
+    return this.audio.currentTime / this.audio.duration;
+  };
+  return playerObj;
 });
